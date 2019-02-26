4331d2ef436633024163717036baa28afaae514f
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-02-26 10:11:14 -0500

    figuring it out

--- ReadFile.java
+++ ReadFile.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class ReadFile {
   public static void main(String args[]) throws FileNotFoundException {
+int i = 0;
+int z = 0;
         //instead of a try/catch, you can throw the FileNotFoundException.
         //This is generally bad behavior
 
@@ -10,11 +12,25 @@ public class ReadFile {
         // can be a path like: "/full/path/to/file.txt" or "../data/file.txt"
 
         //inf stands for the input file
-        Scanner inf = new Scanner(text);
+        Scanner x = new Scanner(text);
+        Scanner y = new Scanner(text);
+        Scanner tex = new Scanner(text);
 
-        while(inf.hasNextLine()){
-            String line = inf.nextLine();
-            System.out.println(line);//hopefully you can do other things with the line
+        while(x.hasNext()){
+            i ++;
         }
-    }
+        while(y.hasNextLine()){
+            z ++;
+        }
+        char [][] hey = new char [i][z];
+        while(tex.hasNextLine()){
+        for (int a = 0; a < i; a ++){
+            for (int b = 0; b < z; b ++){
+                hey [a][b] = tex.nextLine().charAt(b);
+            }
+
+        }}
+        System.out.println(hey);
+}
+
 }
