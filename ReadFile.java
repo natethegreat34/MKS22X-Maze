cec0c3e01fa4a6ee986dcea2c8466781fe698fb6
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-02-26 10:00:39 -0500

    Reads and prints file

---
+++ ReadFile.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class ReadFile {
+  public static void main(String args[]) throws FileNotFoundException {
+        //instead of a try/catch, you can throw the FileNotFoundException.
+        //This is generally bad behavior
+
+        File text = new File("Maze.txt");
+        // can be a path like: "/full/path/to/file.txt" or "../data/file.txt"
+
+        //inf stands for the input file
+        Scanner inf = new Scanner(text);
+
+        while(inf.hasNextLine()){
+            String line = inf.nextLine();
+            System.out.println(line);//hopefully you can do other things with the line
+        }
+    }
+}
